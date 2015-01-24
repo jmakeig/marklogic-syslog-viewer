fee3684cf78882fe5703da98f9c6b85593adf122
Justin Makeig <jmpublic+github@makeig.com>
2015-01-23 23:52:09 -0800

    Sets up REST extension and alert action target module

--- db/setup-extensions.js
+++ db/setup-extensions.js
@@ -9,9 +9,23 @@ db.config.resources.write({
   format: 'javascript',
   source: fs.createReadStream(__dirname + '/extensions/resources/query-alert.sjs'),
   // everything below this is optional metadata
-  title: 'Creates or updates alerts'
+  title: 'Creates or updates alerts',
+  permissions: [
+    { 'role-name': 'logs-reader', capabilities : ['execute'] },
+    { 'role-name': 'logs-writer', capabilities : ['insert', 'update'] }
+  ]
 }).result(function(response) {
   console.log('Installed extension "%s"', response.name);
 }, function(error) {
   console.log(JSON.stringify(error, null, 2));
 });
+
+db.documents.write({
+  uri: 'logs-alert-config/action-push-http.xqy',
+  content: fs.readFileSync(__dirname + '/modules/actions/logs-push-http.xqy', {encoding: 'utf8'}),
+  permissions: [
+    { 'role-name': 'logs-reader', capabilities : ['read'] },
+    { 'role-name': 'logs-writer', capabilities : ['insert', 'update'] }
+  ]
+})
+  .result(function(response) { console.log('Wrote %s', response.documents[0].uri); });
