46e046cfb579b56fa692740beecf139fb38eae54
Justin Makeig <jmpublic+github@makeig.com>
2015-02-20 00:31:58 -0800

    Cleaner output and testing

--- sandbox/facets.js
+++ sandbox/facets.js
@@ -7,21 +7,22 @@ var qb = marklogic.queryBuilder;
 var queryString = '';
 
 var constraints = {
-  "sender": ["MarkLogic"],
-  "severity": ["error", "warning"]
+  sender: ['taskgated'],
+  severity: ['error', 'info', 'notice', 'warning', 'debug']
 }
 
-var where = [
-  qb.parsedFrom(queryString ,
-    qb.parseBindings( 
-      qb.word('host', qb.bind('h')),
-      qb.value('severity', qb.bind('l')),
-      qb.word('sender', qb.bind('s'))
+var where = [qb.collection('logs')];
+if(queryString) {
+  where.push(
+    qb.parsedFrom(queryString,
+      qb.parseBindings( 
+        qb.word('host', qb.bind('h')),
+        qb.value('severity', qb.bind('l')),
+        qb.word('sender', qb.bind('s'))
+      )
     )
-  ),
-  qb.collection('logs')
-];
-
+  );
+}
 function queryFromConstraints(constraints) {
   var where = [];
   if('undefined' === typeof constraints || null === constraints) { return where; }
@@ -42,7 +43,7 @@ function getResults(where, txid) {
     qb.where(
       where.concat(queryFromConstraints(constraints))
     )
-    .slice(5)
+    .slice(1, 4, qb.snippet()) /* First result will be metadata, including snippets, the rest will be the documents */
     .withOptions({txid: txid})
   )
   .result();
@@ -83,19 +84,40 @@ function getTransaction() {
 
 Promise.using(getTransaction(), function(txn) {
   var txid = txn.txid;
+  // console.log('Running in transaction %d', txid);
   Promise.join(
     getResults(where, txid),
     getFacet('sender', where, txid),
     getFacet('host', where, txid),
     getFacet('severity', where, txid),
     function(results, senders, hosts, severities) {
-      console.log('%j', results);
-      console.log('*****************************************');
-      console.log('%j', senders);
-      console.log('*****************************************');
-      console.log('%j', hosts);
-      console.log('*****************************************');
-      console.log('%j', severities);
+      var out = Object.create(null);
+      out.meta = results[0];
+      out.docs = results.slice(1);
+      out.facets = {};
+      out.facets.sender = senders[0].facets.sender;
+      out.facets.host = hosts[0].facets.host;
+      out.facets.severity = severities[0].facets.severity;
+      console.log(JSON.stringify(out));
+      // console.log('%j', results);
+      // console.log('*****************************************');
+      // console.log('%j', senders);
+      // console.log('*****************************************');
+      // console.log('%j', hosts);
+      // console.log('*****************************************');
+      // console.log('%j', severities);
+      
+      // console.log(JSON.stringify(
+      //   Array.prototype.slice.apply(arguments).map(function(r) {
+      //     if(Array.isArray(r) && r.length > 0) {
+      //       var item = r[0];
+      //       if('undefined' !== typeof item.facets) {
+      //         return item.facets;
+      //       }
+      //     }
+      //     return r;
+      //   })
+      // ));
     }
   )
 }).catch(function(error) {
